81b314ead751746082e8ad1b00d0d0bd202220f7
JohnEveryOC <j.every@eagles.oc.edu>
2026-06-02 19:53:45 -0500

    update script

--- getdata.py
+++ getdata.py
@@ -3,10 +3,9 @@ import csv
 import random
 
 # uncomment and set your own paths. Make sure to not include file extension
-sf_path = "D:/AIProj1/rawdata/tx_2020_gen_2020_blocks/tx_2020_gen_2020_blocks"
-csv_train_path = "D:/AIProj1/cleandata/texas_training.csv"
-csv_test_path = "D:/AIProj1/cleandata/texas_testing.csv"
-csv_test_validation_path = "D:/AIProj1/cleandata/texas_testing_validation.csv"
+sf_path = "D:/AIProj1/rawdata/ca_2020_gen_2020_blocks/ca_2020_gen_2020_blocks"
+csv_train_path = "D:/AIProj1/cleandata/california_training.csv"
+csv_test_path = "D:/AIProj1/cleandata/california_testing.csv"
 
 sf = shapefile.Reader(sf_path)
 fields = sf.fields[1:]
@@ -27,7 +26,6 @@ for field in field_names:
 records = sf.shapeRecords()
 train_data = []
 test_data = []
-test_validation_data = []
 for data in records:
     #bbox 0 and 2 are lat data, take avg. Same for 1 and 3
     lat = round((data.shape.bbox[0] + data.shape.bbox[2]) / 2, 7) # 7 because Nix's coords were at 7 decimal places
@@ -38,7 +36,6 @@ for data in records:
     r_vote = 0
     total_votes = 0
     index = 5
-    test_split_rand = random.randint(1, 5) # split 1:4 between test and train. Trying to preemptively prevent overfitting.
 
     for votes in data.record[5:]:
         if index in democrat_fields:
@@ -50,10 +47,7 @@ for data in records:
     if total_votes > 0:
         d_vote = d_vote / total_votes
         r_vote = r_vote / total_votes
-    if test_split_rand == 1:
-        test_data.append([id, lat, long, voting_pop])
-        test_validation_data.append([id, d_vote, r_vote])
-    else:
+        test_data.append([id, lat, long, voting_pop, total_votes])
         train_data.append([id, lat, long, voting_pop, total_votes, d_vote, r_vote])
     
 with open(csv_train_path, 'w', newline='') as csvtrainfile:
@@ -63,7 +57,3 @@ with open(csv_train_path, 'w', newline='') as csvtrainfile:
 with open(csv_test_path, 'w', newline='') as csvtestfile:
     writer = csv.writer(csvtestfile)
     writer.writerows(test_data)
-
-with open(csv_test_validation_path, 'w', newline='') as csvtestvalidationfile:
-    writer = csv.writer(csvtestvalidationfile)
-    writer.writerows(test_validation_data)
