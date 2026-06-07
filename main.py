eced84b02767a4332b912074f65f4a75e83cbc10
JohnEveryOC <j.every@eagles.oc.edu>
2026-06-07 18:49:21 -0500

    add n nearest neighbors

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 import torch
+import scipy
+import csv
 
+INPUT_SIZE = 100
 def main():
     print(f"PyTorch version: {torch.__version__}")
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    print(f"Using device: {device}")
+    
+    state_names = ["california", "missouri", "montana", "oklahoma", "texas"]
+    training_data = []
 
-    x = torch.rand(3, 3, device=device)
-    print("Sample tensor:\n", x)
+    for name in state_names:
+        # Gus you'll probably need to edit this path unless you copy mine
+        file_path = "D:/AIProj1/cleandata/" + name + "_training.csv"
+        with open(file_path, 'r', newline='') as csvtestfile:
+            reader = csv.reader(csvtestfile)
+            for row in reader:
+                training_data.append(row)
+
+    print(len(training_data))
+
+    coordinates = []
+    for block in training_data:
+        x_y = [block[1], block[2]]
+        coordinates.append(x_y)
+    
+    # scipy idea from top answer on https://stackoverflow.com/questions/12923586/nearest-neighbor-search-python
+    block_tree = scipy.spatial.KDTree(coordinates, leafsize=100)
+    result = block_tree.query(coordinates[0], k=INPUT_SIZE)
+    #result[0] is distances, doesn't matter. result[1] has indexes, result[1][0] is self
+    nearest_indexes = result[1]
+
+    print(nearest_indexes)
+
+
+
+
 
 if __name__ == "__main__":
     main()
