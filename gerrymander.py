c1a0b1e142fd5452044d0c91a719e9bdc1ffc32f
JohnEveryOC <j.every@eagles.oc.edu>
2026-06-19 14:51:35 -0500

    change data shape for GA

--- gerrymander.py
+++ gerrymander.py
@@ -13,14 +13,22 @@ def load_csv(path):
     return np.array(rows, dtype=np.float64)
 
 def main():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("predict_csv") # where to get data from
-    ap.add_argument("districts_csv") # where to print district mappings to
-    args = ap.parse_args()
+    #ap = argparse.ArgumentParser()
+    #ap.add_argument("predict_csv") # where to get data from
+    #ap.add_argument("districts_csv") # where to print district mappings to
+    #args = ap.parse_args()
 
-    predicted_data = load_csv(args.predict_csv)
+    predicted_data = load_csv("D:/AIProj1/predict_data/california_predict.csv")
     num_districts = 12
     democrat_districts = 0 #treat republican as num_districts - democrat_districts, 2 variables seems less good
+    for data in predicted_data:
+        data[3] = round(data[4] * data[5]) #r_vote * num_votes
+        data[4] = round(data[4] * data[6]) #d_vote * num_votes
+    predicted_data = np.delete(predicted_data, 5, axis=1) # trim extra columns
+    predicted_data = np.delete(predicted_data, 5, axis=1)
+    #new shape is [id, long, lat, republican_votes, democrat_votes]
+    #worth noting btw that the total is often 1-2 less than the expected vote amount because of rounding. Could do ceiling and have it be more also
+    
     
 
 
